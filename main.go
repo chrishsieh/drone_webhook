508678f97e96d96562326e7a0a8ed0c161241a25
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-03-03 16:20:41 +0800

    Update

--- main.go
+++ main.go
@@ -1,120 +1,223 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
-	"strconv"
-	"strings"
 
-	"github.com/drone/drone-go/drone"
-	"golang.org/x/oauth2"
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "unknown"
 )
 
 func main() {
-	token := os.Getenv("PLUGIN_TOKEN")
-	var repo = drone.Repo{}
-	var build = drone.Build{}
-	var vargs = struct {
-		Urls []string `json:"urls"`
-	}{}
-
-	fmt.Println(repo)
-	fmt.Println(build)
-	fmt.Println(vargs)
-	// data structure
-	//data := struct {
-	//	Repo  plugin.Repo  `json:"repo"`
-	//	Build plugin.Build `json:"build"`
-	//}{repo, build}
-
-	//fmt.Println(data)
-	// json payload that will be posted
-	//payload, err := json.Marshal(&data)
-	//if err != nil {
-	//	os.Exit(1)
-	//}
-	//fmt.Println(payload)
-
-	host := "https://cloud.drone.io"
-	if hostPathArr := strings.Split(os.Getenv("DRONE_BUILD_LINK"), "/"); len(hostPathArr) > 2 {
-		host = hostPathArr[0] + "//" + hostPathArr[2]
-	}
-	repoName := os.Getenv("DRONE_REPO_NAME")
-	repoNamespace := os.Getenv("DRONE_REPO_NAMESPACE")
-	buildNumber, _ := strconv.Atoi(os.Getenv("DRONE_BUILD_NUMBER"))
-	currentStageNumber, _ := strconv.Atoi(os.Getenv("DRONE_STAGE_NUMBER"))
-	currentBranch := os.Getenv("DRONE_COMMIT_BRANCH")
-	onSuccess := os.Getenv("PLUGIN_ON_SUCCESS")
-	if onSuccess == "" {
-		onSuccess = "change"
-	}
-	onFailure := os.Getenv("PLUGIN_ON_FAILURE")
-	if onFailure == "" {
-		onFailure = "always"
-	}
-	currentBuildStatus := os.Getenv("DRONE_BUILD_STATUS")
-
-	// create an http client with oauth authentication.
-	config := new(oauth2.Config)
-	auther := config.Client(
-		oauth2.NoContext,
-		&oauth2.Token{
-			AccessToken: token,
+	app := cli.NewApp()
+	app.Name = "webhook plugin"
+	app.Usage = "webhook plugin"
+	app.Action = run
+	app.Version = version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "method",
+			Usage:  "webhook method",
+			EnvVar: "PLUGIN_METHOD",
+			Value:  "POST",
 		},
-	)
-
-	// create the drone client with authenticator
-	client := drone.NewClient(host, auther)
-
-	if len(repoNamespace) > 0 && len(repoName) > 0 {
-		var showNotify int
-
-		if onSuccess == "change" || onFailure == "change" {
-			var lastBuild int
-			// Get last build information
-			for page, foundLastBuild := 1, 0; page <= 4 && foundLastBuild == 0; page++ {
-				if gotBuildlist, err := client.BuildList(repoNamespace, repoName, drone.ListOptions{page}); err == nil {
-					for _, element := range gotBuildlist {
-						if currentBranch == element.Source && int64(buildNumber) > element.Number {
-							fmt.Println(element.Source + "[" + strconv.FormatInt(element.Number, 10) + "]:" + element.Status)
-							if element.Status == "success" {
-								lastBuild = 1
-							} else {
-								lastBuild = 2
-							}
-							foundLastBuild = 1
-							break
-						}
-					}
-				}
-			}
-
-			if onSuccess == "change" && currentBuildStatus == "success" && lastBuild != 1 {
-				showNotify = 1
-			}
-			if onFailure == "change" && currentBuildStatus != "success" && lastBuild == 1 {
-				showNotify = 1
-			}
-		}
-		if onSuccess == "always" && currentBuildStatus == "success" {
-			showNotify = 1
-		}
-		if onFailure == "always" && currentBuildStatus != "success" {
-			showNotify = 1
-		}
-
-		if showNotify > 0 {
-			if gotBuild, err := client.Build(repoNamespace, repoName, buildNumber); err == nil {
-				for index, element := range gotBuild.Stages {
-					if index != currentStageNumber-1 {
-						fmt.Println(element.Name + ":" + element.Status)
-					}
-				}
-			}
-		}
-	} else {
-		fmt.Println("No Repo or build.")
-		return
+		cli.StringFlag{
+			Name:   "username",
+			Usage:  "username for basic auth",
+			EnvVar: "PLUGIN_USERNAME,WEBHOOK_USERNAME",
+		},
+		cli.StringFlag{
+			Name:   "password",
+			Usage:  "password for basic auth",
+			EnvVar: "PLUGIN_PASSWORD,WEBHOOK_PASSWORD",
+		},
+		cli.StringFlag{
+			Name:   "content-type",
+			Usage:  "content type",
+			EnvVar: "PLUGIN_CONTENT_TYPE",
+			Value:  "application/json",
+		},
+		cli.StringSliceFlag{
+			Name:   "on_success",
+			Usage:  "on_success",
+			EnvVar: "PLUGIN_ON_SUCCESS",
+			Value:  "change",
+		},
+		cli.StringSliceFlag{
+			Name:   "on_failure",
+			Usage:  "on_failure",
+			EnvVar: "PLUGIN_ON_FAILURE",
+			Value:  "always",
+		},
+		cli.StringFlag{
+			Name:   "template",
+			Usage:  "custom template for webhook",
+			EnvVar: "PLUGIN_TEMPLATE",
+		},
+		cli.StringSliceFlag{
+			Name:   "token",
+			Usage:  "drone API token",
+			EnvVar: "PLUGIN_TOKEN",
+		},
+		cli.StringSliceFlag{
+			Name:   "headers",
+			Usage:  "custom headers key map",
+			EnvVar: "PLUGIN_HEADERS",
+		},
+		cli.StringSliceFlag{
+			Name:   "urls",
+			Usage:  "list of urls to perform the action on",
+			EnvVar: "PLUGIN_URLS,WEBHOOK_URLS",
+		},
+		cli.IntSliceFlag{
+			Name:   "valid-response-codes",
+			Usage:  "list of valid http response codes",
+			EnvVar: "PLUGIN_VALID_RESPONSE_CODES",
+		},
+		cli.BoolFlag{
+			Name:   "debug",
+			Usage:  "enable debug information",
+			EnvVar: "PLUGIN_DEBUG",
+		},
+		cli.BoolFlag{
+			Name:   "skip-verify",
+			Usage:  "skip ssl verification",
+			EnvVar: "PLUGIN_SKIP_VERIFY",
+		},
+		cli.StringFlag{
+			Name:   "repo.owner",
+			Usage:  "repository owner",
+			EnvVar: "DRONE_REPO_OWNER",
+		},
+		cli.StringFlag{
+			Name:   "repo.name",
+			Usage:  "repository name",
+			EnvVar: "DRONE_REPO_NAME",
+		},
+		cli.StringFlag{
+			Name:   "commit.sha",
+			Usage:  "git commit sha",
+			EnvVar: "DRONE_COMMIT_SHA",
+		},
+		cli.StringFlag{
+			Name:   "commit.ref",
+			Value:  "refs/heads/master",
+			Usage:  "git commit ref",
+			EnvVar: "DRONE_COMMIT_REF",
+		},
+		cli.StringFlag{
+			Name:   "commit.branch",
+			Value:  "master",
+			Usage:  "git commit branch",
+			EnvVar: "DRONE_COMMIT_BRANCH",
+		},
+		cli.StringFlag{
+			Name:   "commit.author",
+			Usage:  "git author name",
+			EnvVar: "DRONE_COMMIT_AUTHOR",
+		},
+		cli.StringFlag{
+			Name:   "commit.message",
+			Usage:  "commit message",
+			EnvVar: "DRONE_COMMIT_MESSAGE",
+		},
+		cli.StringFlag{
+			Name:   "build.event",
+			Value:  "push",
+			Usage:  "build event",
+			EnvVar: "DRONE_BUILD_EVENT",
+		},
+		cli.IntFlag{
+			Name:   "build.number",
+			Usage:  "build number",
+			EnvVar: "DRONE_BUILD_NUMBER",
+		},
+		cli.IntFlag{
+			Name:   "build.stage",
+			Usage:  "build stage number",
+			EnvVar: "DRONE_STAGE_NUMBER",
+		},
+		cli.StringFlag{
+			Name:   "build.status",
+			Usage:  "build status",
+			Value:  "success",
+			EnvVar: "DRONE_BUILD_STATUS",
+		},
+		cli.StringFlag{
+			Name:   "build.link",
+			Usage:  "build link",
+			EnvVar: "DRONE_BUILD_LINK",
+		},
+		cli.Int64Flag{
+			Name:   "build.started",
+			Usage:  "build started",
+			EnvVar: "DRONE_BUILD_STARTED",
+		},
+		cli.Int64Flag{
+			Name:   "build.created",
+			Usage:  "build created",
+			EnvVar: "DRONE_BUILD_CREATED",
+		},
+		cli.StringFlag{
+			Name:   "build.tag",
+			Usage:  "build tag",
+			EnvVar: "DRONE_TAG",
+		},
+		cli.Int64Flag{
+			Name:   "job.started",
+			Usage:  "job started",
+			EnvVar: "DRONE_JOB_STARTED",
+		},
 	}
 
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	plugin := Plugin{
+		Repo: Repo{
+			Owner: c.String("repo.owner"),
+			Name:  c.String("repo.name"),
+		},
+		Build: Build{
+			Tag:     c.String("build.tag"),
+			Number:  c.Int("build.number"),
+			Event:   c.String("build.event"),
+			Status:  c.String("build.status"),
+			Commit:  c.String("commit.sha"),
+			Ref:     c.String("commit.ref"),
+			Branch:  c.String("commit.branch"),
+			Author:  c.String("commit.author"),
+			Message: c.String("commit.message"),
+			Link:    c.String("build.link"),
+			Started: c.Int64("build.started"),
+			Created: c.Int64("build.created"),
+			Stage:   c.Int("build.stage"),
+		},
+		Job: Job{
+			Started: c.Int64("job.started"),
+		},
+		Config: Config{
+			Method:      c.String("method"),
+			Username:    c.String("username"),
+			Password:    c.String("password"),
+			ContentType: c.String("content-type"),
+			Template:    c.String("template"),
+			Headers:     c.StringSlice("headers"),
+			URLs:        c.StringSlice("urls"),
+			ValidCodes:  c.IntSlice("valid-response-codes"),
+			Debug:       c.Bool("debug"),
+			SkipVerify:  c.Bool("skip-verify"),
+			Token:       c.String("token"),
+			OnSuccess:   c.String("on_success"),
+			OnFailure:   c.String("on_failure"),
+		},
+	}
+
+	return plugin.Exec()
 }
---
+++ plugin.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/drone/drone-template-lib/template"
+)
+
+type (
+	Repo struct {
+		Owner string `json:"owner"`
+		Name  string `json:"name"`
+	}
+
+	Build struct {
+		Tag     string `json:"tag"`
+		Event   string `json:"event"`
+		Number  int    `json:"number"`
+		Commit  string `json:"commit"`
+		Ref     string `json:"ref"`
+		Branch  string `json:"branch"`
+		Author  string `json:"author"`
+		Message string `json:"message"`
+		Status  string `json:"status"`
+		Link    string `json:"link"`
+		Started int64  `json:"started"`
+		Created int64  `json:"created"`
+		Stage   int    `json:"stage"`
+	}
+
+	Config struct {
+		Method      string
+		Username    string
+		Password    string
+		ContentType string
+		Template    string
+		Headers     []string
+		URLs        []string
+		ValidCodes  []int
+		Debug       bool
+		SkipVerify  bool
+		Token       string
+		OnSuccess   string
+		OnFailure   string
+	}
+
+	Job struct {
+		Started int64
+	}
+
+	Plugin struct {
+		Repo   Repo
+		Build  Build
+		Config Config
+		Job    Job
+	}
+)
+
+func (p Plugin) Exec() error {
+	var (
+		buf bytes.Buffer
+		b   []byte
+	)
+
+	if p.Config.Template == "" {
+		data := struct {
+			Repo  Repo  `json:"repo"`
+			Build Build `json:"build"`
+		}{p.Repo, p.Build}
+
+		if err := json.NewEncoder(&buf).Encode(&data); err != nil {
+			fmt.Printf("Error: Failed to encode JSON payload. %s\n", err)
+			return err
+		}
+
+		b = buf.Bytes()
+	} else {
+		txt, err := template.RenderTrim(p.Config.Template, p)
+
+		if err != nil {
+			return err
+		}
+
+		text := txt
+		b = []byte(text)
+	}
+
+	// build and execute a request for each url.
+	// all auth, headers, method, template (payload),
+	// and content_type values will be applied to
+	// every webhook request.
+
+	for i, rawurl := range p.Config.URLs {
+		uri, err := url.Parse(rawurl)
+
+		if err != nil {
+			fmt.Printf("Error: Failed to parse the hook URL. %s\n", err)
+			os.Exit(1)
+		}
+
+		r := bytes.NewReader(b)
+		req, err := http.NewRequest(p.Config.Method, uri.String(), r)
+
+		if err != nil {
+			fmt.Printf("Error: Failed to create the HTTP request. %s\n", err)
+			return err
+		}
+
+		req.Header.Set("Content-Type", p.Config.ContentType)
+
+		for _, value := range p.Config.Headers {
+			header := strings.Split(value, "=")
+			req.Header.Set(header[0], header[1])
+		}
+
+		if p.Config.Username != "" && p.Config.Password != "" {
+			req.SetBasicAuth(p.Config.Username, p.Config.Password)
+		}
+
+		client := http.DefaultClient
+
+		if p.Config.SkipVerify {
+			client = &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: true,
+					},
+				},
+			}
+		}
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			fmt.Printf("Error: Failed to execute the HTTP request. %s\n", err)
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		if p.Config.Debug || resp.StatusCode >= http.StatusBadRequest {
+			body, err := ioutil.ReadAll(resp.Body)
+
+			if err != nil {
+				fmt.Printf("Error: Failed to read the HTTP response body. %s\n", err)
+			}
+
+			if p.Config.Debug {
+				fmt.Printf(
+					debugFormat,
+					i+1,
+					req.URL,
+					req.Method,
+					req.Header,
+					string(b),
+					resp.Status,
+					string(body),
+				)
+			} else {
+				fmt.Printf(
+					respFormat,
+					i+1,
+					req.URL,
+					resp.Status,
+					string(body),
+				)
+			}
+		}
+
+		if len(p.Config.ValidCodes) > 0 && !intInSlice(p.Config.ValidCodes, resp.StatusCode) {
+			return fmt.Errorf("Error: Response code %d not found among valid response codes", resp.StatusCode)
+		}
+
+	}
+
+	return nil
+}
+
+// Function checks if int is in slice of ints
+func intInSlice(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
